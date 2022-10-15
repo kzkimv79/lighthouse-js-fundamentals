82e041297c83e8b329476d0c3504508d64ecb009
Viktor Kim <kzkimv79@gmail.com>
2022-10-15 15:53:56 -0400

    Odd Checker assignment

---
+++ odd.js
@@ -0,0 +1,9 @@
+const isOdd = function (num) {
+return num % 2 !== 0;
+}
+
+console.log("1 is odd: " + isOdd(1));
+console.log("2 is odd: " + isOdd(2));
+console.log("3 is odd: " + isOdd(3));
+console.log("4 is odd: " + isOdd(4));
+console.log("5 is odd: " + isOdd(5))  
