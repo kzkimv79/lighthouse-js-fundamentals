f2b7ce1c875dbfdfa17fb5d3aa5afb4f48ca7a1e
Viktor Kim <kzkimv79@gmail.com>
2022-10-19 19:12:36 -0400

    Banana bread

---
+++ ingredients.js
@@ -0,0 +1,16 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+for(let i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i])
+}
+
+const ingredients1 = ["eggs1", "milk1", "flour1", "sugar1", "baking soda1", "baking powder1", "chocolate chips1", "bananas1"];
+let i = 0;
+while(i < ingredients1.length) {
+  console.log(ingredients1[i]);
+  i++
+}
+
+const ingredients2 = ["eggs2", "milk2", "flour2", "sugar2", "baking soda2", "baking powder2", "chocolate chips2", "bananas2"];
+for (i = ingredients2.length - 1; i >= 0; i--) {
+  console.log(ingredients2[i]);
+}
